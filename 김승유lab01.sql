115a1e5267e11aed28971b2c81cd38d214602b51
huntkiller77@naver.com <>
2018-06-26 15:51:35 +0900

    2018_06_26_김승유_과제1

--- 김승유lab01.sql
+++ 김승유lab01.sql
@@ -88,10 +88,10 @@ SELECT e.EMPNO
 7844	TURNER	0
 -------------------- */
 -- 실습(5)
-SELECT e.EMPNO AS "사번"
-      ,e.ENAME AS "이름"
-      ,e.SAL AS "급여"
-      ,e.HIREDATE AS "입사일"
+SELECT e.EMPNO as "사번"
+      ,e.ENAME as "이름"
+      ,e.SAL as "급여"
+      ,e.HIREDATE as "입사일"
   FROM emp e
 ;
 
@@ -156,7 +156,8 @@ SELECT e.EMPNO
       ,e.SAL
       ,e.DEPTNO
   FROM emp e
-  WHERE e.DEPTNO = 20 AND e.SAL < 3000
+  WHERE e.DEPTNO = 20 
+    AND e.SAL < 3000
 ;
 
 /* -------------------------
@@ -166,7 +167,7 @@ SELECT e.EMPNO
 -- 실습(10)
 SELECT e.EMPNO
       ,e.ENAME
-      ,e.SAL + e.COMM AS "급여와 커미션 합"
+      ,e.SAL + e.COMM as "급여와 커미션 합"
   FROM emp e
 ;
 
@@ -187,7 +188,7 @@ SELECT e.EMPNO
 -- 실습(11)
 SELECT e.EMPNO
       ,e.ENAME
-      ,e.SAL*12 AS "년급여"
+      ,e.SAL*12 as "년급여"
   FROM emp e
 ;
 
@@ -212,8 +213,8 @@ SELECT e.EMPNO
       ,e.SAL
       ,e.COMM
   FROM emp e
-  WHERE e.ENAME = 'MARTIN' OR 
-        e.ENAME ='BLAKE'
+  WHERE e.ENAME = 'MARTIN' 
+     OR e.ENAME ='BLAKE'
 ;
 
 /* ---------------------------------------
@@ -224,10 +225,10 @@ SELECT e.EMPNO
 SELECT e.EMPNO
       ,e.ENAME
       ,e.JOB
-      ,e.SAL + e.COMM AS "급여와 커미션 합"
+      ,e.SAL + e.COMM as "급여와 커미션 합"
   FROM emp e
-  WHERE e.ENAME = 'MARTIN' OR 
-        e.ENAME ='BLAKE'
+  WHERE e.ENAME = 'MARTIN' 
+     OR e.ENAME ='BLAKE'
 ;
 
 /* ------------------------------
@@ -237,7 +238,7 @@ SELECT e.EMPNO
 -- 실습(14)
 SELECT *
   FROM emp e
-  WHERE e.COMM > 0
+  WHERE NOT e.COMM = 0
 ;
 
 /* ----------------------------------------------------------------
@@ -258,13 +259,153 @@ SELECT *
 7844	TURNER	SALESMAN	7698	81/09/08	1500	0	    30
 ---------------------------------------------------------------- */
 -- 실습(16)
+SELECT *
+  FROM emp e
+  WHERE e.DEPTNO = 20 
+    AND e.SAL > 2500
+;
+
+/* --------------------------------------------------------
+7566	JONES	MANAGER	7839	81/04/02	2975		20
+7902	FORD	ANALYST	7566	81/12/03	3000		20
+-------------------------------------------------------- */
 -- 실습(17)
+SELECT *
+  FROM emp e
+  WHERE e.JOB = 'MANAGER' OR 
+        e.DEPTNO = 10
+;
+
+/* --------------------------------------------------------
+7566	JONES	MANAGER	7839	81/04/02	2975		20
+7698	BLAKE	MANAGER	7839	81/05/01	2850		30
+7782	CLARK	MANAGER	7839	81/06/09	2450		10
+7839	KING	PRESIDENT		81/11/17	5000		10
+7934	MILLER	CLERK	7782	82/01/23	1300		10
+-------------------------------------------------------- */
 -- 실습(18)
+SELECT *
+  FROM emp e
+  WHERE e.JOB  IN('MANAGER','CLERK','SALESMAN')
+;
+
+/* ----------------------------------------------------------------
+7369	SMITH	CLERK	    7902	80/12/17	800		        20
+7499	ALLEN	SALESMAN	7698	81/02/20	1600	300	    30
+7521	WARD	SALESMAN	7698	81/02/22	1250	500	    30
+7566	JONES	MANAGER	    7839	81/04/02	2975		    20
+7654	MARTIN	SALESMAN	7698	81/09/28	1250	1400	30
+7698	BLAKE	MANAGER	    7839	81/05/01	2850		    30
+7782	CLARK	MANAGER	    7839	81/06/09	2450		    10
+7844	TURNER	SALESMAN	7698	81/09/08	1500	0	    30
+7900	JAMES	CLERK	    7698	81/12/03	950		        30
+7934	MILLER	CLERK	    7782	82/01/23	1300		    10
+---------------------------------------------------------------- */
 -- 실습(19)
+SELECT *
+  FROM emp e
+  WHERE e.ENAME LIKE 'A%'
+;
+
+/* ----------------------------------------------------------------
+7499	ALLEN	SALESMAN	7698	81/02/20	1600	300	    30
+---------------------------------------------------------------- */
 -- 실습(20)
+SELECT *
+  FROM emp e
+  WHERE e.ENAME LIKE '%A%'
+;
+
+/* ----------------------------------------------------------------
+7499	ALLEN	SALESMAN	7698	81/02/20	1600	300	    30
+7521	WARD	SALESMAN	7698	81/02/22	1250	500	    30
+7654	MARTIN	SALESMAN	7698	81/09/28	1250	1400	30
+7698	BLAKE	MANAGER	    7839	81/05/01	2850		    30
+7782	CLARK	MANAGER	    7839	81/06/09	2450		    10
+7900	JAMES	CLERK	    7698	81/12/03	950		        30
+---------------------------------------------------------------- */
 -- 실습(21)
+SELECT *
+  FROM emp e
+  WHERE e.ENAME LIKE '%S'
+;
+
+/* ---------------------------------------------------------------
+7566	JONES	MANAGER	7839	81/04/02	2975		    20
+7900	JAMES	CLERK	7698	81/12/03	950		        30
+--------------------------------------------------------------- */
 -- 실습(22)
+SELECT *
+  FROM emp e
+  WHERE e.ENAME LIKE '%E_'
+;
+
+/* ----------------------------------------------------------------
+7499	ALLEN	SALESMAN	7698	81/02/20	1600	300  	30
+7566	JONES	MANAGER	    7839	81/04/02	2975		    20
+7844	TURNER	SALESMAN	7698	81/09/08	1500	0	    30
+7900	JAMES	CLERK	    7698	81/12/03	950		        30
+7934	MILLER	CLERK	    7782	82/01/23	1300		    10
+---------------------------------------------------------------- */
 -- 실습(23)
+SELECT *
+  FROM emp e
+  WHERE e.SAL 
+    BETWEEN 2500 
+    AND 3000
+;
+
+/* ---------------------------------------------------------
+7566	JONES	MANAGER	7839	81/04/02	2975		20
+7698	BLAKE	MANAGER	7839	81/05/01	2850		30
+7902	FORD	ANALYST	7566	81/12/03	3000		20
+--------------------------------------------------------- */
 -- 실습(24)
+SELECT *
+  FROM emp e
+  WHERE e.COMM IS NULL
+;
+
+/* ---------------------------------------------------------
+7369	SMITH	CLERK	7902	80/12/17	800		    20
+7566	JONES	MANAGER	7839	81/04/02	2975		20
+7698	BLAKE	MANAGER	7839	81/05/01	2850		30
+7782	CLARK	MANAGER	7839	81/06/09	2450		10
+7839	KING	PRESIDENT		81/11/17	5000		10
+7900	JAMES	CLERK	7698	81/12/03	950		    30
+7902	FORD	ANALYST	7566	81/12/03	3000		20
+7934	MILLER	CLERK	7782	82/01/23	1300		10
+--------------------------------------------------------- */
 -- 실습(25)
+SELECT *
+  FROM emp e
+  WHERE e.COMM IS NOT NULL
+;
+
+/* ------------------------------------------------------------------
+7499	ALLEN	SALESMAN	7698	81/02/20	1600	300	    30
+7521	WARD	SALESMAN	7698	81/02/22	1250	500	    30
+7654	MARTIN	SALESMAN	7698	81/09/28	1250	1400	30
+7844	TURNER	SALESMAN	7698	81/09/08	1500	0	    30
+------------------------------------------------------------------ */
 -- 실습(26)
+SELECT e.EMPNO as 사번
+      ,e.ENAME || '의 월급은 $' || e.SAL || '입니다'  as 월급여
+  FROM emp e 
+;
+
+/* -------------------------------
+사번, 월급여
+7369	SMITH의 월급은 $800입니다
+7499	ALLEN의 월급은 $1600입니다
+7521	WARD의 월급은 $1250입니다
+7566	JONES의 월급은 $2975입니다
+7654	MARTIN의 월급은 $1250입니다
+7698	BLAKE의 월급은 $2850입니다
+7782	CLARK의 월급은 $2450입니다
+7839	KING의 월급은 $5000입니다
+7844	TURNER의 월급은 $1500입니다
+7900	JAMES의 월급은 $950입니다
+7902	FORD의 월급은 $3000입니다
+7934	MILLER의 월급은 $1300입니다
+------------------------------- */
