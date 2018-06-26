84857523e48f8759c6472c99ace6e9df4e66cff5
huntkiller77@naver.com <>
2018-06-26 15:52:11 +0900

    sql day01.sql 오후 4시까지 커밋

--- sql.day01.sql
+++ sql.day01.sql
@@ -242,3 +242,117 @@ SELECT e.EMPNO
 7698	BLAKE	MANAGER	    81/05/01	
 7839	KING	PRESIDENT	81/11/17	
 ------------------------------------------ */
+
+-- (4) Alias : 별칭
+-- 10) emp 테이블에서
+--     empno --> Employee No.
+--     ename --> Employee Name
+--     job   --> job Name
+SELECT e.EMPNO as "Employee No."
+      ,e.ENAME as "Employee Name"
+      ,e.JOB as "job Name"
+  FROM emp e
+;
+
+-- 11) 10번과 동일 as 키워드 생략하여 조회 
+--     empno --> 사번
+--     ename --> 사원 이름
+--     job   --> 직무
+SELECT e.EMPNO as 사번
+      ,e.ENAME as "사원 이름"
+      ,e.JOB as 직무
+  FROM emp e
+;
+
+-- 12) 테이블에 붙이는 별칭을 주지 않았을 때
+SELECT EMPNO
+  FROM emp 
+;
+
+SELECT emp.EMPNO
+  FROM emp 
+;
+
+SELECT e.EMPNO
+  FROM emp e
+;
+
+SELECT d.DEPTNO
+  FROM dept d
+;
+
+-- 13) 영문별칭 사용시 특수기호 _ 사용하는 경우
+SELECT e.EMPNO Employee_No
+      ,e.ENAME "Employee Name" --대소문자 구분을 위해 " "을 씀
+  FROM emp e
+;
+
+-- 14) 별칭과 정렬의 조합 : SELECT 절에 별칭을 준 경우 ORDER BY 절에서 사용가능
+--     emp 테이블에서 사번, 이름, 직무, 입사일, 커미션을 조회할 때
+--     정렬은 커미션, 직무, 이름을 오름차순 정렬
+SELECT e.EMPNO 사번
+      ,e.ENAME 이름
+      ,e.JOB 직무
+      ,e.HIREDATE 입사일
+      ,e.COMM 커미션
+  FROM emp e
+  ORDER BY 커미션, 직무, 이름
+;
+
+-- 15) DISTINCT, 별칭, 정렬의 조합
+-- job 을 중복으로 제거하여 직무 라는 별칭을 조회하고
+-- 내림차순으로 정렬
+SELECT DISTINCT e.JOB as 직무
+  FROM emp e
+  ORDER BY 직무 DESC
+;
+
+-- 16) emp 테이블에서 empno 이 7900인 사원의
+--     사번, 이름, 직무, 입사일, 급여, 부서번호
+SELECT e.EMPNO as 사번
+      ,e.ENAME as 이름
+      ,e.JOB as 직무
+      ,e.HIREDATE as 급여
+      ,e.DEPTNO as 부서번호
+  FROM emp e
+  WHERE e.EMPNO = 7900
+; 
+
+/* ------------------------------------
+7900	JAMES	CLERK	81/12/03	30
+------------------------------------ */
+
+-- 17) emp 테이블에서 empno 는 7900 이거나 deptno 가 20인 직원의 정보를 조회
+--     사번, 이름, 직무, 입사일, 급여, 부서번호
+SELECT e.EMPNO
+      ,e.ENAME
+      ,e.JOB
+      ,e.HIREDATE
+      ,e.SAL
+      ,e.DEPTNO
+  FROM emp e
+  WHERE e.EMPNO = 7900 
+     OR e.DEPTNO = 20
+;
+
+/* --------------------------------------------
+7369	SMITH	CLERK	80/12/17	800	    20
+7566	JONES	MANAGER	81/04/02	2975	20
+7900	JAMES	CLERK	81/12/03	950	    30
+7902	FORD	ANALYST	81/12/03	3000	20
+-------------------------------------------- */
+
+-- 18) 17번의 조회조건을 AND 조건으로 조합
+--     emp 테이블에서 empno 가 7900 이고 deptno 가 20인 직원의 정보를 조회
+--     사번, 이름, 직무, 입사일, 급여, 부서번호
+SELECT e.EMPNO
+      ,e.ENAME
+      ,e.JOB
+      ,e.HIREDATE
+      ,e.SAL
+      ,e.DEPTNO
+  FROM emp e
+  WHERE e.EMPNO = 7900 
+    AND e.DEPTNO = 20
+;
+-- 인출된 모든 행 : 0
